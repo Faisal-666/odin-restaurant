2d45616a42f1741950de2c5b0c95757bc9b85d28
Faisal-666 <Faisalakbar661@gmail.com>
2026-07-22 10:06:15 +0700

    feat: add active highlight to nav btn

--- src/index.js
+++ src/index.js
@@ -15,6 +15,15 @@ document.addEventListener('DOMContentLoaded', () => {
    const content = document.querySelector('#content');
    render(content, 'home', homePage);
 
+   const nav = document.querySelector('nav');
+   nav.addEventListener('click', (e) => {
+    const target = e.target.closest('button');
+    if (!target) return;
+
+    document.querySelector('nav > button.active').classList.remove('active');
+    target.classList.add('active');
+   });
+
    document.addEventListener('click', (e) => {
     const target = e.target.id;
     if (!target) return;
@@ -23,10 +32,14 @@ document.addEventListener('DOMContentLoaded', () => {
         case 'home':
         case 'home-logo':
             render(content, 'home', homePage);
+            document.querySelector('nav > button.active').classList.remove('active');
+            document.querySelector('nav > #home').classList.add('active');
             break;
         case 'menu':
         case 'menuBtn':
             render(content, 'menu', menuPage);
+            document.querySelector('nav > button.active').classList.remove('active');
+            document.querySelector('nav > #menu').classList.add('active');
             break;
         case 'about':
             render(content, 'about', () => '<h2>About</h2>');
